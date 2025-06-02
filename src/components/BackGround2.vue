ad26d5197f6ddbdd3740c780f8f32fa0a18e7026
dgt-longnd-6006 <long.nguyen@digitran.asia>
2025-06-02 12:39:24 +0700

    update

--- src/components/BackGround2.vue
+++ src/components/BackGround2.vue
@@ -34,15 +34,15 @@ import { ref, watch } from 'vue';
 import party from 'party-js';
 
 const images = ref([
-  { src: '/images/slide1.jpg', date: '12-01-2024' },
-  { src: '/images/slide2.jpg', date: '12-01-2024' },
-  { src: '/images/slide4.jpg', date: '12-01-2024' },
-  { src: '/images/slide8.jpg', date: '12-01-2024' },
-  { src: '/images/slide9.jpg', date: '16-01-2025' },
-  { src: '/images/slide3.jpg', date: '08-02-2025' },
-  { src: '/images/slide5.jpg', date: '23-04-2025' },
-  { src: '/images/slide6.jpg', date: '23-04-2025' },
-  { src: '/images/slide7.jpg', date: '23-04-2025' },
+  { src: '/happy-birthday/images/slide1.jpg', date: '12-01-2024' },
+  { src: '/happy-birthday/images/slide2.jpg', date: '12-01-2024' },
+  { src: '/happy-birthday/images/slide4.jpg', date: '12-01-2024' },
+  { src: '/happy-birthday/images/slide8.jpg', date: '12-01-2024' },
+  { src: '/happy-birthday/images/slide9.jpg', date: '16-01-2025' },
+  { src: '/happy-birthday/images/slide3.jpg', date: '08-02-2025' },
+  { src: '/happy-birthday/images/slide5.jpg', date: '23-04-2025' },
+  { src: '/happy-birthday/images/slide6.jpg', date: '23-04-2025' },
+  { src: '/happy-birthday/images/slide7.jpg', date: '23-04-2025' },
 ]);
 const currentSlide = ref(0);
 
